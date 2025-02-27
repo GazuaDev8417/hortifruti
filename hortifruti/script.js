c0180a9a3729627343def74af9fcb69db21a4161
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2025-02-27 10:45:10 -0300

    primeiro commit

---
+++ hortifruti/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typing', {
+    strings: [
+        'Tomate', 'Batata', 'Brócolis', 'Cebola', 'Alface', 'Pepino', 'Pimentão',
+        'Uva', 'Laranja', 'Maçã', 'Limão', 'Melancia', 'Melão', 'Tangerina'
+    ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+})
